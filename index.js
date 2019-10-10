8ff97b37840ace9a9995539fa9a663f3ed21a2a9
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-10-10 13:53:46 +1100

    added some validations to user inputs

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var Word = require("./Word");
 var inquirer = require("inquirer");
 
+var rounds;
+var newWord
+
 //testing
-var newWord = new Word("test");
+//var newWord = new Word("test");
 // console.log(newWord.word)
 // console.log(newWord.letters())
 // console.log(newWord.wordDisplay())
 
-//start game
-var rounds = 0;
-newWord.letters();
-indexWordDisplay();
-userGuess();
+startGame("test");
 
+
+//start game function
+function startGame(word){
+    newWord = new Word(word);
+    rounds = 0;
+    newWord.letters();
+    indexWordDisplay();
+    userGuess();
+}
 
 function indexWordDisplay(){
     newWord.wordDisplay();
@@ -29,28 +37,57 @@ function userGuess(){
                 type: "input"
             }
         ]).then(function(answer){
-            
-            newWord.guess(answer.guess);
-            rounds++
-           
-            var re = new RegExp(answer.guess, "g")
 
-            if(!newWord.display.match(/_/g)){
-                console.log("You guessed it!")
-                indexWordDisplay();
-
-            } else if(!newWord.display.match(re)){
-                console.log("\n------------------------------------\nIncorrect guess, try again...")
-                indexWordDisplay();
-                userGuess(); 
-
+            if (!answer.guess){
+                console.log("...you forgot to pick a letter, type a letter THEN press enter...")
+                userGuess();
             }else{
-                console.log("\n------------------------------------\nCorrect guess, keep going!")
-                indexWordDisplay();
-                userGuess(); 
+
+                
+                var validGuess = answer.guess.toLowerCase();
+                newWord.guess(validGuess);
+                rounds++
+                
+                var re = new RegExp(validGuess, "g")
+                
+                if(!newWord.display.match(/_/g)){
+                    console.log("You guessed it!")
+                    indexWordDisplay();
+                    playAgain();
+                    
+                } else if(!newWord.display.match(re)){
+                    console.log("\n------------------------------------\nIncorrect guess, try again...\n")
+                    indexWordDisplay();
+                    userGuess(); 
+                    
+                }else{
+                    console.log("\n------------------------------------\nCorrect guess, keep going!\n")
+                    indexWordDisplay();
+                    userGuess(); 
+                }
+                
             }
-            
         });
         
+    }else{
+        console.log("\n... you didn't guess it.\nThe word was: " + newWord.word + "\nBit too tricky, try again!\n");
+        playAgain();
+    }
+
+    function playAgain(){
+        inquirer.prompt([
+            {
+                message: "Play again?",
+                name: "confirm",
+                type: "confirm"
+            }
+        ]).then(function(answer){
+            if(answer.confirm){
+                console.log("\n\n...loading another round...\n\n")
+                startGame("second");
+            }else{
+                console.log("\nok bye!\n")
+            }
+        })
     }
 }
